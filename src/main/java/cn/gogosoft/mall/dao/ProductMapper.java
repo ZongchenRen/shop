647c493256a5b382aad10d22deb2e4c0935386cc
ZongchenRen <rzcbj@outlook.com>
2020-02-27 22:33:03 +0800

    商品列表--完结

---
+++ src/main/java/cn/gogosoft/mall/dao/ProductMapper.java
@@ -0,0 +1,25 @@
+package cn.gogosoft.mall.dao;
+
+import java.util.List;
+import java.util.Set;
+
+import org.apache.ibatis.annotations.Param;
+
+import cn.gogosoft.mall.pojo.Product;
+
+public interface ProductMapper {
+	int deleteByPrimaryKey(Integer id);
+
+	int insert(Product record);
+
+	int insertSelective(Product record);
+
+	Product selectByPrimaryKey(Integer id);
+
+	int updateByPrimaryKeySelective(Product record);
+
+	int updateByPrimaryKey(Product record);
+
+	// CategoryIdSet 和xml中的collection对应
+	List<Product> selectByCategoryIdSet(@Param("CategoryIdSet") Set<Integer> CategoryIdSet);
+}
